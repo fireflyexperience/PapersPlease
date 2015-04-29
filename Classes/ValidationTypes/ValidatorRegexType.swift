0ffe0b06d3f98c6ba9211ff444160732b4554b89
Andy Bennett <andy@steamshift.net>
2015-04-29 14:22:41 +0100

    fix issue with utf16 count

--- Classes/ValidationTypes/ValidatorRegexType.swift
+++ Classes/ValidationTypes/ValidatorRegexType.swift
@@ -9,33 +9,33 @@
 import Foundation
 
 class ValidatorRegexType:ValidatorType {
-    
+
     var regexString = ""
-    
+
     override func isTextValid(text: String) -> Bool {
-        
+
         var error:NSError? = nil
-        
+
         self.valid = false
-        
+
         if let regex = NSRegularExpression(pattern: self.regexString, options: .CaseInsensitive, error: &error) {
-        
-            let num_matches:Int = regex.numberOfMatchesInString(text, options: .ReportProgress, range: NSMakeRange(0, text.utf16Count))
-        
+
+            let num_matches:Int = regex.numberOfMatchesInString(text, options: .ReportProgress, range: NSMakeRange(0, count(text.utf16)))
+
             self.valid = (num_matches == 1)
-            
+
         }
-        
+
         // update states
         self.validationStates.removeAll()
         (self.valid) ? (self.validationStates.append(self.status.valid))
                      : (self.validationStates.append(self.status.invalid))
-        
+
         return self.valid
     }
-    
+
     class override func type() -> String {
         return "ValidationTypeRegex"
     }
-    
+
 }
